c316427694cedbec4b86ce006fc84c5f2704c25e
misssrakiep <shameerarakiep@gmail.com>
2017-09-26 12:51:28 +0200

    cross origin nonsense

--- server.js
+++ server.js
@@ -21,6 +21,14 @@ app.use(bodyParser.json());
 //getting data from server
 // ===============================================================
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', '"Origin, X-Requested-With, Content-Type, Accept"');
+  next();
+})
+
+
 app.get('/', function(req, res){
   res.redirect('/api/shoes');
 })
