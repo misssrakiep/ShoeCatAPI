a8352d68debfdbf0f4c4cefbd8acf9771de01770
misssrakiep <shameerarakiep@gmail.com>
2017-09-29 10:01:41 +0200

    stuff

--- shoeCat.js
+++ shoeCat.js
@@ -106,7 +106,7 @@ module.exports = function(models) {
     //find shoes, sell them and update stock count
     var id = req.params._id;
 
-    models.shoes.findById(req.params._id, function(err, shoes){
+    models.shoes.findById(req.params.id, function(err, shoes){
       if (err){
         res.send(err);
       }
